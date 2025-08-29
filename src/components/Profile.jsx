d8f84e17fa44a4e89f85c0bf6928646cda72963e
Patrick Crown <patrickcrownmilliss@gmail.com>
2025-08-29 11:41:40 +1000

    finished profile banner

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,38 @@
+import { useState } from 'react'
+import Confetti from 'react-confetti'
+import { useWindowSize } from 'react-use'
 export default function Profile() {
+    const [level, setLevel] = useState(42)
+    function increaseLevel() {
+        setLevel(prevLevel => prevLevel + 1);
+    }
+
+    
+    const { width, height } = useWindowSize()
+    const confettiAdapted = <Confetti width={width} height={height}/>
+    
     return (
-        <>
-            <section>Hello</section>
-        </>
+        <section id='profile-block'>
+            {level == 69 && confettiAdapted}
+            <section className="profile-header">
+                <img src='CodingCockatoo.jpg' alt='profile icon'></img>
+                <section className="name-description">
+                    <h2>Coding Cockatoo</h2>
+                    <p class='name'>Patrick Crown-Milliss <img src="au.gif" alt='aus flag'></img> Sydney, Australia </p>
+                    <p>Welcome to my profile! Have a great day friendo.</p>
+                </section>
+                <section className="level">
+                    <div>Level <button onClick={increaseLevel}><span class="dot">{level}</span></button></div>
+                    <div id='badge'>
+                        <img src='UNSW.png' alt='UNSW Logo'></img>
+                        <p>
+                            Computer Science
+                            <p class='xp'>222 XP</p>
+                        </p>
+                        
+                    </div>
+                </section>
+            </section>
+        </section>
     )
 }
