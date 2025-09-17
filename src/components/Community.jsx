e8fb0bbf080d830db0ec36fc8050a5682fc774e7
Patrick Crown <patrickcrownmilliss@gmail.com>
2025-09-17 11:09:24 +1000

    added construction indicator for wishlist and videos

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,3 +1,3 @@
 export default function Wishlist() {
-    return <h1>Videos go here</h1>
+    return <h1 style={{color: "white", textAlign:"center"}}>UNDER CONSTRUCTION - Please come back later!</h1>
 }
--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,3 +1,3 @@
 export default function Wishlist() {
-    return <h1>Wishlist goes here</h1>
+    return <h1 style={{color: "white", textAlign:"center"}}>UNDER CONSTRUCTION - Please come back later!</h1>
 }
